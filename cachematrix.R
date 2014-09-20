5a3a36a7a851ee2e8cdf012b778beb5b69c3e5aa
konstml <konstml@users.noreply.github.com>
2014-09-20 15:26:46 -0400

    Update cachematrix.R
    Prg assgn 2 as of 15:26 09/20/2014

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,89 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## "makeCacheMatrix" # creates a special "matrix" object that can 
+#   cache its inverse.  This is achieved via creating a list containing 
+#  1.  a function to set the value of the matrix
+#  2.  get the value of the matrix
+#  3.  set the value of the inverse
+#  4.  get the value of the inverse
+#   
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  if (length(grep("MASS", (.packages())))==0)  {
+    library(MASS)     # for computing generalized inverse, their `ginv` is 
+    #  a  convenient     interface to `svd`
+  }
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvrs <- function(inv) m <<- inv
+  #     if(dim)
+  #     m <<- expression(obj)
+  #   }
+  getinvrs <- function() m
+  list(set = set, get = get,
+       setinvrs = setinvrs,
+       getinvrs = getinvrs)
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve" calculates the inverse of a matrix
+#  it first checks to see if the inverse has already been calculated. 
+#  If so, it `get`'s the inverse  from the  cache and skips the computation. 
+#  Otherwise, it attempts to calculate  the inverse of
+#  the data and sets the value of the inverse in the cache via the "setinvrs"
+#  function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinvrs()
+  if(!is.null(m)) {
+    message("getting cached inverse of a matrix")
+    return(m)
+  }
+  data <- x$get()
+  
+  cmpInv <- function(dt) {
+    out <- tryCatch(  { if (dim(dt)[1]==dim(dt)[2])  solve(data, ...)  
+                        else
+                          ginv(dt,...) 
+    },
+    error= function(cond) {
+      #  message(paste("Your matrix:", as.character(head(data))))
+      ?message("Failed to invert supplied matrix; original error message:")
+      message(cond)                    
+      return(NA)  # Choose a return value in case of error
+    } 
+    )
+    return(out)
+  }
+  
+  m <- cmpInv(data) 
+  if (is.numeric(m))  {
+    x$setinvrs(m)   # cache the obtained inverse
+    ## Return a matrix that is the inverse of 'x'
+    return(m)   
+  }
+  else    return(out)   # NA, the message has been generated
+  #   and sent to stderr()  connection
+  
 }
+
+  ################# Usage:   ##################
+ #  Regular pos. def. matrix:
+ # hilbert <- function(n) { i <- 3:n; 1 / outer(i - 1, i, "+") }
+ # h4 <- hilbert(6) 
+ # specM <- makeCacheMatrix(h4)
+ #  t1 <- cacheSolve(specM2)
+
+  # Rectangular matrix: 
+
+     #  m2 <- matrix( abs(rnorm(12, 250,38)),3,4) 
+     # specM2 <- makeCacheMatrix(m2)
+   #  chk <- cacheSolve(specM2)
